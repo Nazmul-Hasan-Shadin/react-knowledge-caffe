75a295854ef143573b6e9c4780bbbe5da5fbb7de
NazmulHasan000 <nazmulhasanshadin000@gmail.com>
2023-09-12 00:22:56 +0600

    blogs project updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
+  const [bookmarks,setBookmarks]=useState([]);
+  const [readingTime,setReadingTime]=useState(0);
 
+  const handleAddToBookMark=blog=>{
+   const newBookmarks=[...bookmarks,blog];
+   setBookmarks(newBookmarks);
+  }
+
+  const handleMarkAsRead=(id,time)=>{
+   setReadingTime(readingTime+time);
+  //  remove the red blog from bookmark
+   const remainningBookmark= bookmarks.filter(bookmark=>bookmark.id !==id)
+   setBookmarks(remainningBookmark)
+
+
+
+  }
 
   return (
     <>
 
    <Header></Header>
-    <div className='md:flex'>
-      <Blogs></Blogs>
-       <Bookmarks></Bookmarks>
+    <div className='md:flex max-w-7xl mx-auto'>
+      <Blogs handleMarkAsRead={handleMarkAsRead} handleAddToBookMark={handleAddToBookMark}></Blogs>
+       <Bookmarks readingTime={readingTime} bookmarks={bookmarks}></Bookmarks>
 
     </div>
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <header className='flex justify-between items-center p-4 mx-4 border-b-2 '>
+        <header className='flex justify-between items-center p-4  border-b-2 max-w-7xl mx-auto '>
             <h2 className='text-4xl'>KNOWLEDGE CAFE</h2>
             <img src={profile} alt="" />
         </header>
